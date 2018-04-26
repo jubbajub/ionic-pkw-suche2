f14c8fd65008e6dc932fb8ad4c1da0717d3d9da9
jubbajub <sefer.derya@gmx.de>
2018-04-26 19:10:04 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,15 @@ import { ListPage } from '../pages/list/list';
   templateUrl: 'app.html'
 })
 export class MyApp {
+  
   @ViewChild(Nav) nav: Nav;
 
   rootPage: any = HomePage;
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{
+    title: string, 
+    component: any}
+    >;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
