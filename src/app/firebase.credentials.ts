20a3263a052f092a4fbf80f241b5930a64398c44
jubbajub <sefer.derya@gmx.de>
2018-04-27 21:04:12 +0200

    firebase credentials DONE

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBZwN5XtZk0Kp80QVeHNL6Lk5NKK9xlJuk",
+    authDomain: "ionic-pkw-suche2.firebaseapp.com",
+    databaseURL: "https://ionic-pkw-suche2.firebaseio.com",
+    projectId: "ionic-pkw-suche2",
+    storageBucket: "",
+    messagingSenderId: "932791564246"
+  };
